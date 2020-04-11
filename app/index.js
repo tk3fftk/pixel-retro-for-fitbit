717f53cd7ecc7005a3acadd16818ffbbf583f74a
Hiroki Takatsuka <tktk.stereoman@gmail.com>
2020-04-11 18:06:53 +0900

    Digit (#1)
    * add numbers
    
    * update images
    
    * update steps as score
    
    * update time
    
    * refactor

--- app/index.js
+++ app/index.js
@@ -1,46 +1,88 @@
-import clock from "clock";
-import document from "document";
-import { preferences } from "user-settings";
-import * as util from "../common/utils";
-import { HeartRateSensor } from "heart-rate";
-import { today } from "user-activity";
+import clock from 'clock';
+import document from 'document';
+import { preferences } from 'user-settings';
+import * as util from '../common/utils';
+import { HeartRateSensor } from 'heart-rate';
+import { today } from 'user-activity';
+
+const imageNamePrefix = '32x32_';
+const imageExtension = '.png';
+const score1 = document.getElementById('score1');
+const score2 = document.getElementById('score2');
+const score3 = document.getElementById('score3');
+const score4 = document.getElementById('score4');
+const score5 = document.getElementById('score5');
+const hours1 = document.getElementById('hours1');
+const hours2 = document.getElementById('hours2');
+const minutes1 = document.getElementById('minutes1');
+const minutes2 = document.getElementById('minutes2');
 
 let currentHR = 0;
 
 if (HeartRateSensor) {
   const hrm = new HeartRateSensor({ frequency: 1 });
-  hrm.addEventListener("reading", () => {
+  hrm.addEventListener('reading', () => {
     currentHR = hrm.heartRate;
     const numberOfHeart = currentHR / 10;
-    console.log(`Current heart rate: ${currentHR}`);
 
     for (let i = 1; i <= 15; i++) {
-      const isHidden = numberOfHeart >= i ? "visible" : "hidden";
+      const isHidden = numberOfHeart >= i ? 'visible' : 'hidden';
       document.getElementById(`h${i}`).style.visibility = isHidden;
     }
 
-    document.getElementById("score").text = today.adjusted.steps;
+    updateScore();
   });
   hrm.start();
 }
 
 // Update the clock every minute
-clock.granularity = "minutes";
-
-// Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
+clock.granularity = 'minutes';
 
 // Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
+clock.ontick = evt => {
+  const d = evt.date;
+  const hours = d.getHours();
+  if (preferences.clockDisplay === '12h') {
     // 12h format
     hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
   }
-  let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  // const mins = util.zeroPad(d.getMinutes());
+  const mins = d.getMinutes();
+  setOne(mins, minutes1);
+  setTen(mins, minutes2);
+  setOne(hours, hours1);
+  setTen(hours, hours2);
 };
+
+function updateScore() {
+  const steps = today.adjusted.steps;
+  setOne(steps, score1);
+  setTen(steps, score2);
+  setHundred(steps, score3);
+  setThousand(steps, score4);
+  setTenThousand(steps, score5);
+}
+
+function setOne(val, target) {
+  drawDigit(Math.floor(val % 10), target);
+}
+
+function setTen(val, target) {
+  drawDigit(Math.floor(Math.floor(val % 100) / 10), target);
+}
+
+function setHundred(val, target) {
+  drawDigit(Math.floor(Math.floor(val % 1000) / 100), target);
+}
+
+function setThousand(val, target) {
+  drawDigit(Math.floor(Math.floor(val % 10000) / 1000), target);
+}
+
+function setTenThousand(val, target) {
+  drawDigit(Math.floor(val / 10000), target);
+}
+
+function drawDigit(val, target) {
+  target.image = `${imageNamePrefix}${val}${imageExtension}`;
+}
